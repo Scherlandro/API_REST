79841874f81f629606a56f51e87e4aae5ec7be8a
Space-iu <scherlandro@gmail.com>
2025-07-06 16:19:05 -0300

    Atualizando a @Query em FuncionarioRepository

--- biontec-api/src/main/java/com/biontecapi/repository/FuncionarioRepository.java
+++ biontec-api/src/main/java/com/biontecapi/repository/FuncionarioRepository.java
@@ -13,14 +13,12 @@ public interface FuncionarioRepository extends JpaRepository<Funcionario,Integer
 
     List<Funcionario> findByFirstName(String firstName);
 
-    @Query("SELECT f FROM funcionario f WHERE LOWER(TRIM(f.nome_funcionario)) LIKE LOWER(CONCAT('%', TRIM(':name'), '%')) "+
-    " AND f.salario > :salario ORDER BY f.salario DESC, f.nome_funcionario ASC")
-    List<Funcionario> findBySalaryOrderBySalario(@Param("nome_funcionario") String name, @Param("salario") BigDecimal salario);
+    @Query("SELECT f FROM Funcionario f WHERE LOWER(TRIM(f.nome_funcionario)) LIKE LOWER(CONCAT('%', TRIM(':name'), '%')) "+
+    " AND f.salario > :amount ORDER BY f.salario DESC, f.nome_funcionario ASC")
+    List<Funcionario> buscarPorValorSalarial(@Param("name") String name, @Param("amount") BigDecimal salario);
 
 @Query("SELECT e FROM Funcionario e WHERE lower(e.nome_funcionario) LIKE lower(concat('%', :name, '%')) AND e.salario > :salario ORDER BY e.nome_funcionario ASC")
-List<Funcionario> findBySalaryOrderByName(String name, BigDecimal salario);
-
-// SELECT * FROM funcionario WHERE LOWER(TRIM(nome_funcionario)) LIKE LOWER(CONCAT('%', TRIM('Sc'), '%')) AND salario > 1300 ORDER BY salario DESC, nome_funcionario ASC;
+List<Funcionario> listarSalarioPorFuncionario(String name, BigDecimal salario);
 
 
 }
