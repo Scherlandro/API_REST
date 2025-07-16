1a87d9806ecc8701ac30a139c0b696bd45c8f25d
Space-iu <scherlandro@gmail.com>
2025-07-15 23:18:25 -0300

    Update ClienteControll

--- biontec-api/src/main/java/com/biontecapi/controller/ClienteController.java
+++ biontec-api/src/main/java/com/biontecapi/controller/ClienteController.java
@@ -1,15 +1,29 @@
 package com.biontecapi.controller;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.biontecapi.dtos.ClienteDTO;
 import com.biontecapi.model.Cliente;
 import com.biontecapi.service.ClienteService;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -44,6 +58,7 @@ public class ClienteController {
     }
 
     @PostMapping(path = "/salvar")
+    @ResponseStatus(HttpStatus.CREATED)
     public Cliente salvar(@RequestBody Cliente cliente){
         return clienteService.save(cliente);
     }
