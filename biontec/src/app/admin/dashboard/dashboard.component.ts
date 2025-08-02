76238c320e11bdfc74f0a06372a4cc62b08c40e1
Space-iu <scherlandro@gmail.com>
2025-08-02 17:05:46 -0300

    Update dashbord

--- biontec/src/app/admin/dashboard/dashboard.component.ts
+++ biontec/src/app/admin/dashboard/dashboard.component.ts
@@ -1,29 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCalendar} from "@angular/material/datepicker";
 import {FormControl, FormGroup} from "@angular/forms";
-import {MatTableDataSource} from "@angular/material/table";
+import {MatTable, MatTableDataSource} from "@angular/material/table";
 import {IUser} from "../../interfaces/user";
 import {DialogUsuarioComponent} from "../../shared/diolog_components/dialog-usuario/dialog-usuario.component";
 import {MensagemService} from "../../services/mensagem.service";
-import {Observable} from "rxjs";
+import {Observable, of} from "rxjs";
 import {NotificationMgsService} from "../../services/notification-mgs.service";
+import {MatPaginator, PageEvent} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {iProduto} from "../../interfaces/product";
+import {DomSanitizer, SafeUrl} from "@angular/platform-browser";
+import {ProductService} from "../../services/product.service";
+import {ShoppingCartService} from "../../services/shopping-cart.service";
+import {MatDialog} from "@angular/material/dialog";
+import {catchError} from "rxjs/operators";
+import {ErrorDiologComponent} from "../../shared/diolog_components/error-diolog/error-diolog.component";
 
-
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
-export interface IEvent{
-
-}
-
-const today = new Date();
-const month = today.getMonth();
-const year = today.getFullYear();
 
 @Component({
   selector: 'app-dashboard',
@@ -35,86 +30,113 @@ export class DashboardComponent implements OnInit {
   events = new FormControl();
   mensagens!: Observable<string>  ;
 
+  @ViewChild(MatTable) tableProduto!: MatTable<any>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  pageEvent!: PageEvent;
+  cardProduts= new Observable<iProduto[]>;
+  tbSourceProdutos$ = new MatTableDataSource<iProduto>();
+  produtosFiltered: iProduto[] = [];
+  products: iProduto[] = [];
+  produtoControl = new FormControl();
+  searchTerm !:any;
+  imageUrl: SafeUrl | undefined;
 
-  constructor(private mensagemService: MensagemService,
-              public notificationMsg:  NotificationMgsService
+  constructor(
+              public notificationMsg:  NotificationMgsService,
+              private prodService: ProductService,
+              private cartService: ShoppingCartService,
+              public dialog: MatDialog,
+              private sanitizer: DomSanitizer
               ) { }
 
-  ngOnInit(): void {  }
+  ngOnInit(): void {
+    this.listarProdutos();
+  }
+
+
+  listarProdutos(){
+    this.prodService.getTodosProdutos()
+      .pipe(catchError(error => {
+        this.onError('Erro ao buscar produto.')
+        return of([])}))
+      .subscribe(  (rest: iProduto[])=>  {
+        this.tbSourceProdutos$.data = rest;
+        /*  this.tbSourceProdutos$.paginator = this.paginator;*/
+      } );
+  }
+  getImageUrl(fotoProduto: string): SafeUrl {
+    if (!fotoProduto) return '';
+    const objectURL = 'data:image/jpeg;base64,' + fotoProduto;
+    return this.sanitizer.bypassSecurityTrustUrl(objectURL);
+  }
+
+  consultarPorCod(codProd: string){
+    if (this.produtoControl.valid) {
+      this.prodService.getProdutoPorCod(codProd)
+        .pipe(catchError(error => {
+          this.onError('Erro ao buscar produto.')
+          return of([]) })).subscribe((result:iProduto[]) => {
+        this.tbSourceProdutos$.data = result;
+        console.log("Retorno da MatTableDat ", result )
+      } )
+    }
+  }
+
+  consultarPorNome(nomeProd: string){
+    if (this.produtoControl.valid) {
+      this.prodService.listarProdutoPorNome(nomeProd)
+        .pipe(catchError(error => {
+          this.onError('Erro ao buscar produto.')
+          return of([]) }))
+        .subscribe((result:iProduto[]) => {
+          this.aplicarFiltro(nomeProd);
+          this.tbSourceProdutos$.data = result;
+          console.log(result)
+        } )
+    }
+  }
+
+  aplicarFiltro(valor: string) {
+    valor = valor.trim().toLowerCase();
+    this.tbSourceProdutos$.filter = valor;
+  }
+
+  loginAndAddProd(){
+    //  this.router.navigate(['/auth/login']);
+  }
+
+  changeProdutos(value: any){
+    if (value) {
+      this.produtosFiltered = this.products.filter(products => products.idProduto.toString()
+        .includes(value.toUpperCase()));
+    } else {
+      this.produtosFiltered = this.products;
+    }
+  }
+
+  onError(errrorMsg: string) {
+    this.dialog.open(ErrorDiologComponent, {
+      data: errrorMsg
+    });
+  }
+
+  search(event:any){
+    this.searchTerm = (event.target as HTMLInputElement).value;
+    console.log(this.searchTerm);
+    this.cartService.search.next(this.searchTerm);
+  }
 
   showNotification(){
     this.notificationMsg.openConfirmDialog('Hello')
       .afterClosed().subscribe(res =>{
-        if (res){
-          this.notificationMsg.openConfirmDialog("Beleza");
-        }
+      if (res){
+        this.notificationMsg.openConfirmDialog("Beleza");
+      }
     });
-   // this.mensagens = this.mensagemService.getNotification();
+    // this.mensagens = this.mensagemService.getNotification();
     /*   this.mensagemService.getMensagens().subscribe((mensagem) => this.mensagens.push(mensagem)  );*/
   }
 
-  tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: '#413968'},
-    {text: 'Two', cols: 1, rows: 2, color: 'rgba(21,99,97,0.66)'},
-    {text: 'Three', cols: 1, rows: 1, color: '#5e0d4a'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
-
-  campaignOne = new FormGroup({
-    start: new FormControl(new Date(year, month, 13)),
-    end: new FormControl(new Date(year, month, 16)),
-  });
-
-  campaignTwo = new FormGroup({
-    start: new FormControl(new Date(year, month, 15)),
-    end: new FormControl(new Date(year, month, 19)),
-  });
-
-/*
-listEvents(event: string )
-{
-  this.events = ()[
-    {
-     event.start: new FormControl((new Date()), 1),
-      end: new FormControl(new Date(), 1),
-      title: 'A 3 day event',
-      color: {...colors['red']},
-      actions: this.actions,
-      allDay: true,
-      resizable: {
-        beforeStart: true,
-        afterEnd: true,
-      },
-      draggable: true,
-    },
-      {
-        start: startOfDay(new Date()),
-        title: 'An event with no end date',
-        color: {...colors['yellow']},
-        actions: this.actions,
-      },
-      {
-        start: subDays(endOfMonth(new Date()), 3),
-        end: addDays(endOfMonth(new Date()), 3),
-        title: 'A long event that spans 2 months',
-        color: {...colors['blue']},
-        allDay: true,
-      },
-      {
-        start: addHours(startOfDay(new Date()), 2),
-        end: addHours(new Date(), 2),
-        title: 'A draggable and resizable event',
-        color: {...colors['yellow']},
-        actions: this.actions,
-        resizable: {
-          beforeStart: true,
-          afterEnd: true,
-        },
-        draggable: true,
-      }
-    ]
-}
-
-*/
 
 }
