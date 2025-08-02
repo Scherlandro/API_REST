920d0c0037e4911e471e3fc8d19283064c95ad41
Space-iu <scherlandro@gmail.com>
2025-08-02 17:37:22 -0300

    Update home

--- biontec/src/app/public/home/home.component.ts
+++ biontec/src/app/public/home/home.component.ts
@@ -1,6 +1,5 @@
 import {Component, ElementRef, OnInit, Renderer2, ViewChild, ViewEncapsulation} from '@angular/core';
-import {MensagemService} from "../../services/mensagem.service";
-import {Observable} from "rxjs";
+
 
 @Component({
   selector: 'app-home',
@@ -9,25 +8,12 @@ import {Observable} from "rxjs";
   encapsulation:ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
-@ViewChild('imgMain') imgMain!: ElementRef;
-imgWidth:any=0; imgHeight:any=0;
-zoom=2;
-glassHeight=0;
-glassWidth=0;
-glass:any;
-/*mensagens!: Observable<string> ;
-mensagemService!: MensagemService;*/
 
-  constructor(
-              private elRef: ElementRef, private renderer: Renderer2) {}
+  constructor( ) {}
 
   ngOnInit() {
-  //this.getNotification() ;
   }
-  onImageLoad(e: any){
-  this.imgWidth=(this.imgMain.nativeElement as HTMLImageElement ).width;
-  this.imgHeight=(this.imgMain.nativeElement as HTMLImageElement).height;
-  }
+
 
 
 }
