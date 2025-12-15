7e54ce383378eafc2753d2defa02c77408260006
Space-iu <scherlandro@gmail.com>
2025-12-15 12:44:42 -0300

    Clean Interceptor

--- biontec/src/app/services/interceptors/token.interceptor.ts
+++ biontec/src/app/services/interceptors/token.interceptor.ts
@@ -21,32 +21,24 @@ export class TokenInterceptor implements HttpInterceptor {
   ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    // console.log(request)
+
     const token = this.tokenService.getToken();
     const requestUrl: Array<any> = request.url.split('/');
     const apiUrl: Array<any> = environment.API_PATH.split('/');
 
-    // if (token && requestUrl[2] === apiUrl[2]) {
-    // token para inserir no headers
     if(token !== null){
       let clone = request.clone({
-     // setHeaders: {Authorization:`Bearer ${token}`,token: `${token}`}
         headers: request.headers.set('Authorization', 'Bearer '+token)
       });
- //  https://balta.io/blog/login-logout-protecao-de-rotas-envio-de-tokens-com-angular
-//  https://stackoverflow.com/questions/61081250/angular-9-dependency-injection-error-the-class-cannot-be-created-via-dependen
-     //  console.log('Headers do Interceptor-->',clone)
+
       return next.handle(clone).pipe(
         catchError(error => {
           console.log('No catchError do Interceptor',error)
-   //  if (error instanceof HttpErrorResponse && error.status === 401){
           if(error.status === 401){
             this.tokenService.clearTokenExpired();
-            // this.authService.logout();
           }
           this.apiErrorService.sendError(error.error.message)
           return throwError('Session Expired');
-          //  return throwError(error.message);
         }) )  }
     return next.handle(request);
   }
